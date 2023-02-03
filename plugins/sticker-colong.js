e659f0a860e4701ebde0b51fe5c171c9c02355a9
Yuanda <104593357+hay4tod@users.noreply.github.com>
2023-02-03 21:23:44 +0700

    Update sticker-colong.js

--- plugins/sticker-colong.js
+++ plugins/sticker-colong.js
@@ -9,8 +9,8 @@ try {
     if (/image|video/.test(mime)) {
       let img = await q.download()
       if (!img) throw 'Reply stiker nya!'
-      stiker = await sticker(img, false, 'By', 'NexBotz')
-    } else if (args[0]) stiker = await sticker(false, args[0], 'By', 'NexEp')
+      stiker = await sticker(img, false, 'By', 'Shino • Bot')
+    } else if (args[0]) stiker = await sticker(false, args[0], 'By', 'Yuuu')
   } finally {
     if (stiker) conn.sendFile(m.chat, stiker, 'sticker.webp', '', m)
     else throw 'Conversion failed'
