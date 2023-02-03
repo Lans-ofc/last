35edfc23e78d919fe99fc8e08f71aae7fe6ec44b
Yuanda <104593357+hay4tod@users.noreply.github.com>
2023-02-03 20:59:03 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -3,12 +3,12 @@ let handler = async (m, { conn, text, usedPrefix }) => {
 
     let img = fs.readFileSync('./thumbnail.jpg');
     let img2 = `https://telegra.ph/file/8f7e2b662ac8dc4f7f562.jpg`
-    return conn.sendButton(m.chat, 'Pm Owner aja \n❏ https://wa.me/628812743180 ', 'Makasih, thanks for you donate...', img2, [["MENU", usedPrefix + "menu"], ["OWNER", usedPrefix + "owner"]], m, {
+    return conn.sendButton(m.chat, 'Pm Owner aja \n❏ https://wa.me/62895404330700 ', 'Makasih, thanks for you donate...', img2, [["MENU", usedPrefix + "menu"], ["OWNER", usedPrefix + "owner"]], m, {
         contextInfo: {
             externalAdReply: {
-                mediaUrl: "https://instagram.com/saep.02",
+                mediaUrl: "https://instagram.com/yuuanda_",
                 mediaType: "VIDEO",
-                title: 'Donasi To NexEp ',
+                title: 'Donasi To Yuuu ',
                 body: bottime,
                 previewType: 0,
                 thumbnail: img
