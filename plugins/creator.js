1be4940bc60f18e7a9347282befa4b37cc3a2d6e
Yuanda <104593357+hay4tod@users.noreply.github.com>
2023-02-03 21:49:45 +0700

    Update creator.js

--- plugins/creator.js
+++ plugins/creator.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, usedPrefix, text, args, command }) => {
 let who = m.mentionedJid && m.mentionedJid[0] ? m.mentionedJid[0] : m.fromMe ? conn.user.jid : m.sender
 let name = await conn.getName(who)
 
-let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp;Yuuu\nNICKNAME:👑 Owner Shino • Bot\nORG:NexEp\nTITLE:soft\nitem1.TEL;waid=62895404330700:+62 895-4043-30700\nitem1.X-ABLabel:📞 Nomor Owner\nitem2.URL:https://chat.whatsapp.com/F8r8aVeEl7PAiQyAmzIrmr\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET: soc.markett@gmail.com\nitem3.X-ABLabel:💌 Mail Owner Shino • Bot\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel:📍 Lokasi Saya\nBDAY;value=date:🔖 15 11 2004\nEND:VCARD`
+let vcard = `BEGIN:VCARD\nVERSION:3.0\nN:WhatsApp;Yuuu\nNICKNAME:👑 Owner Shino • Bot\nORG:Yuu\nTITLE:soft\nitem1.TEL;waid=62895404330700:+62 895-4043-30700\nitem1.X-ABLabel:📞 Nomor Owner\nitem2.URL:https://chat.whatsapp.com/F8r8aVeEl7PAiQyAmzIrmr\nitem2.X-ABLabel:💬 More\nitem3.EMAIL;type=INTERNET: soc.markett@gmail.com\nitem3.X-ABLabel:💌 Mail Owner Shino • Bot\nitem4.ADR:;;🇮🇩 Indonesia;;;;\nitem4.X-ABADR:💬 More\nitem4.X-ABLabel:📍 Lokasi Saya\nBDAY;value=date:🔖 15 11 2004\nEND:VCARD`
 const tag_own = await conn.sendMessage(m.chat, { contacts: { displayName: wm, contacts: [{ vcard }] }}, { quoted: fkon })
 let caption = `👋 Hai *${name} @${who.split("@")[0]}*, Nih Owner *${conn.user.name}* kak`
     await conn.reply(m.chat, caption, tag_own, { mentions: conn.parseMention(caption) })
